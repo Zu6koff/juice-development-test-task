babdb4aab8095f817cb8ceb552b4a173f2c253a4
Zu6koff <zu6koff@mail.ru>
2022-12-01 19:53:33 +0300

    added error handling

--- src/components/AsyncDataWrapper/AsyncDataWrapper.jsx
+++ src/components/AsyncDataWrapper/AsyncDataWrapper.jsx
@@ -4,8 +4,8 @@ import { PanelSpinner, Placeholder } from '@vkontakte/vkui';
 const AsyncDataWrapper = ({state, children, placeholder = <PanelSpinner/>}) => {
     if (state === 'loading') return placeholder;
     if (state === 'error') return (
-        <Placeholder title={'Произошла ошибка при загрузке данных'}>
-            Перезапустите приложение
+        <Placeholder>
+            Произошла ошибка при загрузке данных
         </Placeholder>
     );
 
--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -1,4 +1,4 @@
-import { action, makeAutoObservable, runInAction } from 'mobx';
+import { makeAutoObservable, runInAction } from 'mobx';
 import bridge from '@vkontakte/vk-bridge';
 
 class UserStore {
@@ -16,8 +16,8 @@ class UserStore {
     constructor() {
         makeAutoObservable(this);
 
-        this.fetchUserDataFromVK()
-        this.fetchFriends()
+        this.fetchUserDataFromVK();
+        this.fetchFriends();
     }
 
     fetchUserDataFromVK = async () => {
@@ -35,13 +35,20 @@ class UserStore {
     };
 
     fetchFriends = async () => {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        const friends = await response.json();
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+            if (!response.ok) throw new Error('Что-то пошло не так');
+            const friends = await response.json();
 
-        runInAction(() => {
-            this.friends = friends;
-            this.state.friends = 'success';
-        });
+            runInAction(() => {
+                this.friends = friends;
+                this.state.friends = 'success';
+            });
+        } catch (error) {
+            runInAction(() => {
+                this.state.friends = 'error';
+            });
+        }
     };
 
     get full_name() {
